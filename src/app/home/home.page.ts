54376bf1e18ce453af9c1b587f3b948970f06b13
MayuX142 <37704115+MayuX142@users.noreply.github.com>
2021-04-12 23:28:25 +0200

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoadingController } from "@ionic/angular";
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,54 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  discordMessageTitle: string;
+  discordMessageBody: string;
+
+  constructor(public loadingController : LoadingController) {}
+
+  async submitProcess() {
+    const loading = await this.loadingController.create({
+      spinner: "dots",
+      message: 'Envoi du message',
+      translucent: true,
+      cssClass: 'custom-class custom-loading',
+      backdropDismiss: false
+    });
+    await loading.present();
+
+    this.sendDiscordMessage(this.discordMessageTitle, this.discordMessageBody);
+    this.discordMessageTitle = "";
+    this.discordMessageBody = "";
+
+    await loading.dismiss();
+  }
+
+  sendDiscordMessage(title, body) {
+    let request = new XMLHttpRequest();
+    request.open("POST", "https://discord.com/api/webhooks/831264753364107274/CB8uBu44bcUJKQLI3CL4mxh8TR3vDGpKhZL9xZrAPtvLY_u6iosMZXAkDfyV5pkSuO0l");
+    request.setRequestHeader("Content-type", "application/json");
+
+    var embedMessage = {
+      author: {
+        name: "📱 Envoyé depuis l'application mobile"
+      },
+      title: title,
+      description: body,
+      color: this.hexToDecimal("#a98e8e")
+    }
+
+    var message = {
+      embeds: [ embedMessage ]
+    }
+
+    request.send(JSON.stringify(message));
+  }
+
+  hexToDecimal(hex){
+    return parseInt(hex.replace("#", ""), 16);
+  }
+
+
+
 
 }
